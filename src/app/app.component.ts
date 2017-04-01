d61fd122c6e70200346d05ee507b582688968dfc
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-04-01 12:42:06 +0200

    Added a simple google chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  pieChartOptions =  {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Task', 'Hours per Day'],
+      ['Work',     11],
+      ['Eat',      2],
+      ['Commute',  2],
+      ['Watch TV', 2],
+      ['Sleep',    7]
+    ],
+    options: {'title': 'Tasks'},
+  };
+  
 }
