d87dee6f38159015804a41dbb1dbd9e112336acc
sinelykke <seje_sine@hotmail.com>
2017-04-02 00:31:11 +0200

    now it is happyyyyyy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,7 @@ export class AppComponent implements OnInit {
       console.log(this.dataTables);
       this.pieChartOptions.dataTable[1][1] = this.dataTables[0].focus;
       this.pieChartOptions.dataTable[2][1] = this.dataTables[0].procrastination;
-      for (let i = 0; i < 3; i++ ) {
+      for (let i = 0; i < dataTables.length; i++ ) {
         this.barChartOptions.dataTable[i + 1][0] = this.dataTables[i].task;
         this.barChartOptions.dataTable[i + 1][1] = this.dataTables[i].focus;
         this.barChartOptions.dataTable[i + 1][2] = this.dataTables[i].procrastination;
